67152781dbd5802e8ecc54fdda62c4e30069d427
dimitree <dmitriytsyplenkov@gmail.com>
2020-07-10 19:49:14 +0300

    Added Spring Data JPA Service for Visits. Closes #47

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -7,7 +7,7 @@ import javax.persistence.MappedSuperclass;
 public class Person extends BaseEntity {
     @Column(name = "first_name")
     private String firstName;
-    @Column(name = ="last_name")
+    @Column(name ="last_name")
     private String lastName;
 
     public String getFirstName() {
