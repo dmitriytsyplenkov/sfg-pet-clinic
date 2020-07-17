16c64bc20f6ef9c9ebe98447f0e68250d773b9c6
dmitriytsyplenkov <dmitriytsyplenkov@gmail.com>
2020-07-17 23:41:41 +0300

    Fixed pet visit date validation and pet birthday date validation. Closes #67.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VisitController.java
@@ -11,6 +11,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
+
 @Controller
 public class VisitController {
     private final VisitService visitService;
@@ -24,6 +27,12 @@ public class VisitController {
     @InitBinder
     public void setAllowedFields(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                setValue(LocalDate.parse(text));
+            }
+        });
     }
 
     /**
